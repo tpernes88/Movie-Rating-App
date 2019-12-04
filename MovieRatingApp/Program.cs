24bbe0dc3b3fb37283ee9e1508c596d69e05cea4
Tiago Pernes <pernes.9@emea.teleperformance.com>
2019-12-04 11:41:58 +0000

    Project started

---
+++ MovieRatingApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MovieRatingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
